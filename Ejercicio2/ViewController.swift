698738e3c4c5c302105958a82929af1c05641565
franciscoillacases <francisco.illacases@cieep.com>
2023-10-26 09:15:56 +0200

    Initial Commit

---
+++ Ejercicio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Francisco Illa Cases on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
